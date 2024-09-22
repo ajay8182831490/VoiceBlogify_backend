ddf5ad12f9b52db9a36b47e81cc51eeb8000afd4
Ajay Kumar Gupta <77184625+ajay8182831490@users.noreply.github.com>
2024-09-22 19:40:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     secure: process.env.NODE_ENV === 'production', 
     httpOnly: true,
-    sameSite:  'None', domain: '.voiceblogify.netlify.app'
+    sameSite:  'None'
   },
 }));
 
