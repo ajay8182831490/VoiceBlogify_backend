5199bd73943893e15299e328555943c275a790ab
Ajay Kumar Gupta <77184625+ajay8182831490@users.noreply.github.com>
2024-09-22 16:54:00 +0530

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ export const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated() && req.user) {
    console.log(req.user);
     req.userId = req.user.id;
-  console.log("not authenicated);
+  console.log("not authenicated");
 
     return next();
   }
